6bc75b3922aef012d9389f29c1b7c8a21983f7c3
Stone Gu <stone_gu@hotmail.com>
2019-08-05 22:42:35 -0400

    DB design

--- WebContent/bizislife/sql/tables.sql
+++ WebContent/bizislife/sql/tables.sql
@@ -8,29 +8,46 @@ USE bizislife;
 
 #######################################################
 #------------------------------------------------------
+# companytype can be 1 for ProspectingCompany,
+# 2 for RegistedCompany
+#
 # PROSPECTINGCOMPANY keeps all companies registed and not registed.
-# Registed company has realId and registrationdate information
-# Application can scan this table to send out email and phone 
+# Registed company has registname and registrationdate information
+# Application can scan PROSPECTINGCOMPANY table to send out email  
 #    to all companies not registed yet!
-# The ID is used to regist the company in our system.
-# Unregisted company also can use this ID as login key to try our system.
+# The prefername is used to regist the company in this system. 
+#    for unregisted company(prefername_123456 : 123456 is random 6 digit).
+# Unregisted company will have a guest user (guest : guest) in SSO to login
 #------------------------------------------------------
-CREATE TABLE `bizislife`.`PROSPECTINGCOMPANY` ( 
-`ID` VARCHAR(36) NOT NULL, 
-`NAME` VARCHAR(255) NOT NULL, 
-`EMAIL` VARCHAR(50),
-`PHONE` VARCHAR(30),
-`REALMID` VARCHAR(36),
-`REGISTRATIONDATE` DATETIME DEFAULT NULL,
-PRIMARY KEY (`ID`) 
-) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_unicode_ci; 
+
+CREATE TABLE `company` (
+  `ID` bigint(20) NOT NULL,
+  `REALMID` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `PREFERNAME` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `REGISTNAME` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `EMAIL` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `REGISTRATIONDATE` datetime DEFAULT NULL,
+  `REGISTEDCOMPANYID` varchar(36) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `companytype` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
 
 #######################################################
+#------------------------------------------------------
+CREATE TABLE `address` (
+  `ID` bigint(20) NOT NULL,
+  `STREET` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `CITY` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `PROVINCE` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `POSTALCODE` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `COUNTRY` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `LATITUDE` float(10,6) DEFAULT NULL,
+  `LONGITUDE` float(10,6) DEFAULT NULL,
+  `COMPANYID` bigint(20) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_company` (`COMPANYID`),
+  CONSTRAINT `fk_company` FOREIGN KEY (`COMPANYID`) REFERENCES `company` (`ID`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
 
-CREATE TABLE `bizislife`.`COMPANY` ( 
-`ID` VARCHAR(36) NOT NULL, 
-`REALMID` VARCHAR(36) NOT NULL, 
-`PREFERNAME` VARCHAR(255) NOT NULL, 
-`REGISTNAME` VARCHAR(255), 
-PRIMARY KEY (`ID`) 
-) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_unicode_ci; 
+#------------------------------------------------------
